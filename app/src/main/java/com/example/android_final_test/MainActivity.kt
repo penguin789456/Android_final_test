336fafb5eba9cecdad6fad0449b5b609bb28b199
11010131 <11010131@gm.chihlee.edu.tw>
2023-12-28 15:26:22 +0800

    location init

--- app/src/main/java/com/example/android_final_test/MainActivity.kt
+++ app/src/main/java/com/example/android_final_test/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.android_final_test
 
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var location:Location //宣告location
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mLocationManager = getSystemService(LOCATION_SERVICE) as LocationManager //取得location manager
     }
 }
