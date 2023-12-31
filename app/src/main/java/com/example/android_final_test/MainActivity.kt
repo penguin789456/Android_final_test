a51fa20861b99881a13fcf4d5f5c3898e6b46e20
Jerrykai <k86887961@gmail.com>
2023-12-31 20:44:48 +0800

    completed found nearest event
    完成找到距離最近活動的功能

--- app/src/main/java/com/example/android_final_test/MainActivity.kt
+++ app/src/main/java/com/example/android_final_test/MainActivity.kt
@@ -13,13 +13,22 @@ import androidx.core.app.ActivityCompat
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity(),LocationListener {
+    data class Event(val name: String, val longitude: Double, val latitude: Double)  //創建Event資料型態 儲存活動位置
 
     private lateinit var mLocationManager: LocationManager      //LocationManager
     private var hasGPS:Boolean = false          //檢查是否有GPS
-    private var eventDistance: Int? = null      //活動距離(公尺)
+    private var eventDistance: Float? = null      //活動距離(公尺)
     private var isInEventRange:Boolean = false  //判斷是否有在範圍內
     private var nowLocation = Location("nowLocation")       //現在位置
     private var eventLocation = Location("eventLocation")   //活動位置
+
+    //test events 之後改由sql匯入
+    private val events = listOf(
+        Event("event1",-122.080816,37.421306),
+        Event("event2",-122.0802,37.42132),
+        Event("event3", -122.078932,37.421435),
+        Event("event4", -122.082862,37.422066)
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,17 +38,52 @@ class MainActivity : AppCompatActivity(),LocationListener {
     }
 
     override fun onLocationChanged(p0: Location) {
-        //目前經緯度
-        nowLocation.longitude = p0.longitude
-        nowLocation.latitude = p0.latitude
-        //活動經緯度
-        eventLocation.longitude =  -122.080816  //之後改為由sql匯入Event經緯度(用function回傳)
-        eventLocation.latitude = 37.421306
+        getNowLocation(p0.latitude,p0.longitude)                             //目前經緯度
+        val nearestEvent = findNearestEvent(events)
+        getEventLocation(nearestEvent!!.latitude,nearestEvent.longitude)     //活動經緯度
+        eventDistance = nowLocation.distanceTo(eventLocation)           //取得與活動間的距離
+        isInEventRange = inEventRange(eventDistance!!.toInt())          //判斷是否在活動範圍內
 
-        getDistanceToEvent( eventLocation.longitude,eventLocation.latitude)  //距離在eventDistance
-        isInEventRange = inEventRange(eventDistance!!)  //判斷是否在活動範圍內
+        Log.d("LTag","nowLongitude:${nowLocation.longitude} \nnowLatitude:${nowLocation.latitude} ")
+        Log.d("LTag","eventName:${nearestEvent.name} \neventLongitude:${nearestEvent.longitude} \neventLatitude:${nearestEvent.latitude}  \nDistance:${eventDistance}m\n" +
+                                "inEventRange:${isInEventRange}")
+    }
 
-        Log.d("LTag","longitude${nowLocation.longitude},latitude${nowLocation.latitude} \n inEventRange:${isInEventRange}")
+    //取得現在位置
+    private fun getNowLocation(latitude: Double, longitude: Double) {
+        nowLocation.longitude = longitude
+        nowLocation.latitude = latitude
+    }
+
+    //取得活動位置
+    private fun getEventLocation(latitude: Double, longitude: Double) {
+        eventLocation.longitude = longitude
+        eventLocation.latitude = latitude
+    }
+
+    //判斷是否進入活動範圍
+    private fun inEventRange(distance:Int): Boolean {
+        //距離活動小於50m 回傳true
+        return distance < 50
+    }
+
+    //找出最近的活動
+    private fun findNearestEvent(events: List<Event>): Event? {
+        var nearestEvent: Event? = null
+        var minDistance = Float.MAX_VALUE
+
+        for(event in events){
+            val location = Location("event")
+            location.latitude = event.latitude
+            location.longitude = event.longitude
+
+            val distance = nowLocation.distanceTo(location)
+            if(distance < minDistance){
+                minDistance = distance
+                nearestEvent = event
+            }
+        }
+        return nearestEvent
     }
 
     //檢查location權限
@@ -56,25 +100,6 @@ class MainActivity : AppCompatActivity(),LocationListener {
         }else{
             Log.d("LTag","設備未提供定位服務")  //如果沒找到GPS
         }
-    }
-
-    //取得與活動間的距離
-    private fun getDistanceToEvent(longitude:Double,latitude:Double) {
-        //計算距離
-        eventDistance = nowLocation.distanceTo(eventLocation).toInt()
-        Log.d("LTag","eventDistance:${eventDistance}")
-    }
-
-    //判斷是否進入活動範圍
-    private fun inEventRange(distance:Int): Boolean {
-        //距離活動小於50m 回傳true
-        return distance < 50
-    }
-
-    //取得活動位置
-    private fun getEventLocation(){
-        //透過sql回傳所有經緯度，然後使用透過比大小，將距離我們最近的活動訊息傳回來。
-        //sql 
     }
 
     override fun onResume() {
