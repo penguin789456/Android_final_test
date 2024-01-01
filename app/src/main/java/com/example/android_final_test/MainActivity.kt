d1dfa103bb21cb6c62d423f4b457601edcfb1017
AVES <binhome219@gmail.com>
2024-01-01 21:04:53 +0800

    csv reader init

--- app/src/main/java/com/example/android_final_test/MainActivity.kt
+++ app/src/main/java/com/example/android_final_test/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.android_final_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val inputStream: InputStream = resources.openRawResource(R.raw.acd4f2fa19cd40ed)
+        val reader = BufferedReader(InputStreamReader(inputStream))
+        var line: String?
+        while (reader.readLine().also { line = it } != null) {
+            // 處理每一行的數據
+        }
+        reader.close()
+
     }
 }
