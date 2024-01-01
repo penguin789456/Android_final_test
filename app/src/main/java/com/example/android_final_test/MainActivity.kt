a841c4f2d175d86ae0acc041041ae19d329f2c38
Zhiying <melody9306@gmail.com>
2024-01-01 20:18:08 +0800

    fragment init

--- app/src/main/java/com/example/android_final_test/MainActivity.kt
+++ app/src/main/java/com/example/android_final_test/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.android_final_test
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDet = findViewById<Button>(R.id.btnDetail)
+        val btnLink = findViewById<Button>(R.id.btnLink)
+
+        /*val myName = arrayOf("XXX","OOO","QQQ","YYY","ZZZ")
+
+        val bundle = Bundle()
+        bundle.putStringArray("myDate",myName)
+
+        val fragment = Fragment_advertise()
+        fragment.arguments = bundle*/
+
+        btnDet.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainerView3,Fragment_advertise())
+                commit()
+            }
+        }
+
+
+
     }
 }
