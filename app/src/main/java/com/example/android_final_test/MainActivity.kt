f40de525ceaa466471965a3e94475f6edc57fd8e
TATTTTTTT <liu7134@gmail.com>
2024-01-03 14:32:31 +0800

    Fin_commit_allshow_textview_button

--- app/src/main/java/com/example/android_final_test/MainActivity.kt
+++ app/src/main/java/com/example/android_final_test/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         recyclerview.layoutManager = LinearLayoutManager(this)//通知管理者
 
         val Dataa = ArrayList<oneitem>()//從int到oneitem(因名字變成oneitem)
-    for (i in 0..15) {
+    for (i in 0..9) {
         Dataa.add(oneitem("station","information","Button $i","LinkButton $i"))
     }
         val adapter = recyclerviewAdapter(Dataa)//從上述Data放到這裡,但需要自訂義東西來去繼承
